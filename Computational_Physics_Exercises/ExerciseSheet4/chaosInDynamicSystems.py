3313993bdf64a9f7d3fb5a2dc8d29116a13fba2b
Bl4ckP4nther4 <bl4ckp4nther4@gmail.com>
2020-05-15 23:37:37 +0500

    Added class to generate time evolution sequences

--- Computational_Physics_Exercises/ExerciseSheet4/chaosInDynamicSystems.py
+++ Computational_Physics_Exercises/ExerciseSheet4/chaosInDynamicSystems.py
@@ -2,45 +2,72 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class pendulumDiffEq():
+class freePendulumDiffEq():
     omega0 = 1  # angular frequency of the pendulum for small angles
     alpha = 0   # starting angle
     f = 0       # driving force
     omega = 0   # angular frequency of the driving force
     h = 0.1     # length of one time step
 
-    Y_1 = 0.1   # initial position
-    Y_2 = 0     # initial speed
+    Y = np.zeros(2)  # vector Y
+    Y[0] = 0.1  # initial position
+    Y[1] = 0   # initial speed
+
+    def __init__(self, omega0):
+        self.omega0 = omega0
+        self.alpha = 0
+        self.f = 0
+        self.Y = np.zeros(2)  # vector Y
+        self.Y[0] = 0.1  # initial position
+        self.Y[1] = 0   # initial speed
+
+    def diffEq(self, t, Y):
+        # system of linked differential equations
+        F = np.zeros(2)
+        F[0] = Y[1]
+        F[1] = -self.omega0**2 * \
+            np.sin(Y[0]) - self.alpha*Y[1] + self.f * np.cos(self.omega * t)
+        return F
 
 
-Y = np.array([Y_1, Y_2])  # vector Y
+class timeEvolutionFreePendulum(freePendulumDiffEq):
+    tMax = 10
+    h = freePendulumDiffEq.h
+    timeEvolution = np.zeros((int(tMax/h), 2))
+
+    def __init__(self, omega0, tMax):
+        self.omega0 = omega0
+        self.alpha = 0
+        self.f = 0
+        self.Y = np.zeros(2)  # vector Y
+        self.Y[0] = 0.1  # initial position
+        self.Y[1] = 0   # initial speed
+
+        self.tMax = tMax
+
+        self.setTimeEvolution()
+
+    def RK4(self, t, Y):
+        # Runge Kutta Methode
+        k1 = self.h * self.diffEq(t, Y)
+        k2 = self.h * self.diffEq(t + self.h/2, Y + k1/2)
+        k3 = self.h * self.diffEq(t + self.h/2, Y + k2/2)
+        k4 = self.h * self.diffEq(t + self.h, Y + k3)
+        return Y + k1/2 + k2/3 + k3/3 + k4/6
+
+    def setTimeEvolution(self):
+        # calculate position and speed for all timesteps
+
+        self.timeEvolution = np.zeros((int(self.tMax/self.h), 2))
+        Y = self.Y
+        for i in range(int(self.tMax/self.h)):
+            t = i*self.h
+            Y = self.RK4(t, Y)
+
+            self.timeEvolution[i, 0] = t
+            self.timeEvolution[i, 1] = Y[0]
 
 
-def F(t, Y):
-    # system of linked differential equations
-    F_1 = Y[1]
-    F_2 = -omega0**2 * np.sin(Y[0]) - alpha*Y[1] + f * np.cos(omega * t)
-    return np.array([F_1, F_2])
-
-
-def RK4(Y, t, h):
-    # Runge Kutta Methode
-    k1 = h * F(t, Y)
-    k2 = h * F(t + h/2, Y + k1/2)
-    k3 = h * F(t + h/2, Y + k2/2)
-    k4 = h * F(t + h, Y + k3)
-    return Y + k1/2 + k2/3 + k3/3 + k4/6
-
-
-# calculate position and speed for all timesteps
-points = np.zeros((int(10/h), 2))
-for i in range(int(10/h)):
-    t = i*h
-    print(t)
-    Y = RK4(Y, t, h)
-    points[i, 0] = t
-    points[i, 1] = Y[0]
-
-# plot the results from one
-plt.plot(points[:, 0], points[:, 1])
+exercise1a1 = timeEvolutionFreePendulum(1, 10)
+plt.plot(exercise1a1.timeEvolution[:, 0], exercise1a1.timeEvolution[:, 1])
 plt.show()
