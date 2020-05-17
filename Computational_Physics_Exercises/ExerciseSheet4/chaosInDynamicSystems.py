bf1eaa26a583f02f8528fcb01ff56ccf09526a7a
Bl4ckP4nther4 <bl4ckp4nther4@gmail.com>
2020-05-17 13:07:53 +0500

    Restructuring: Class diffEq, Meth:  plot, setTime

--- Computational_Physics_Exercises/ExerciseSheet4/chaosInDynamicSystems.py
+++ Computational_Physics_Exercises/ExerciseSheet4/chaosInDynamicSystems.py
@@ -3,93 +3,94 @@ import matplotlib.pyplot as plt
 import random as rand
 
 
-class diffEqFreePendulum():
-    alpha = 0   # friction
-    f = 0       # driving force
-    omega = 0   # frequency of the driving force
+class diffEqPendulum():
+    __omega0 = 1  # angular frequency of the pendulum for small angles
+    __alpha = 0   # friction
+    __f = 0       # driving force
+    __omega = 0   # frequency of the driving force
 
-    omega0 = 1  # angular frequency of the pendulum for small angles
-    h = 0.1     # length of one time step
+    __tMax = 10       # total time length
+    __tStep = 0.1     # length of one time step
 
-    Y_0 = np.zeros(2)  # vector Y
-    Y_0[0] = 0.1  # initial position
-    Y_0[1] = 0   # initial speed
+    __Y_0 = np.zeros(2)  # vector Y
+    __Y_0[0] = 0.1  # initial position
+    __Y_0[1] = 0   # initial speed
 
-    def __init__(self, omega0):
-        self.omega0 = omega0
-
-    def diffEq(self, t, Y):
-        # system of linked differential equations
-        F = np.zeros(2)
-        F[0] = Y[1]
-        F[1] = -self.omega0**2 * \
-            np.sin(Y[0]) - self.alpha*Y[1] + self.f * np.cos(self.omega * t)
-        return F
-
-
-class timeEvolutionFreePendulum(diffEqFreePendulum):
-    tMax = 10
     timeSeq = np.zeros(100)
     positionSeq = np.zeros(100)
     speedSeq = np.zeros(100)
 
-    def __init__(self):
-        self.setTimeEvolution(self.Y_0[0], self.Y_0[1])
+    def __init__(self, omega0, alpha, f, omega):
+        self.__omega0 = omega0
+        self.__alpha = alpha
+        self.__f = f
+        self.__omega = omega
 
-    def RK4(self, t, Y):
+    def setTimings(self, tMax, tStep):
+        self.__tMax = tMax
+        self.__tStep = tStep
+
+    def __diffEq(self, t, Y):
+        # system of linked differential equations
+        F = np.zeros(2)
+        F[0] = Y[1]
+        F[1] = -self.__omega0**2 * \
+            np.sin(Y[0]) - self.__alpha*Y[1] + \
+            self.__f * np.cos(self.__omega * t)
+        return F
+
+    def __RK4(self, t, Y, tStep):
         # Runge Kutta Methode
-        k1 = self.h * self.diffEq(t, Y)
-        k2 = self.h * self.diffEq(t + self.h/2, Y + k1/2)
-        k3 = self.h * self.diffEq(t + self.h/2, Y + k2/2)
-        k4 = self.h * self.diffEq(t + self.h, Y + k3)
+        k1 = tStep * self.__diffEq(t, Y)
+        k2 = tStep * self.__diffEq(t + tStep/2, Y + k1/2)
+        k3 = tStep * self.__diffEq(t + tStep/2, Y + k2/2)
+        k4 = tStep * self.__diffEq(t + tStep, Y + k3)
         return Y + k1/6 + k2/3 + k3/3 + k4/6
 
-    def setTimeEvolution(self, initPos, initSpeed):
-        # calculate position and speed for all timesteps
-        self.Y_0[0] = initPos
-        self.Y_0[1] = initSpeed
+    def __setTimeEvolution(self, initPos, initSpeed):
+        self.__Y_0[0] = initPos
+        self.__Y_0[1] = initSpeed
 
-        self.timeSeq = np.zeros(int(self.tMax/self.h))
-        self.positionSeq = np.zeros(int(self.tMax/self.h))
-        self.speedSeq = np.zeros(int(self.tMax/self.h))
+        self.timeSeq = np.zeros(int(self.__tMax/self.__tStep))
+        self.positionSeq = np.zeros(int(self.__tMax/self.__tStep))
+        self.speedSeq = np.zeros(int(self.__tMax/self.__tStep))
 
-        Y = self.Y_0
-        for i in range(int(self.tMax/self.h)):
-            t = i*self.h
-            Y = self.RK4(t, Y)
+        Y = self.__Y_0
+        for i in range(int(self.__tMax/self.__tStep)):
+            t = i*self.__tStep
+            Y = self.RK4(t, Y, self.__tStep)
 
             self.timeSeq[i] = t
             self.positionSeq[i] = Y[0]
             self.speedSeq[i] = Y[1]
 
-    def timePlot(self):
+    def timePlot(self, initPos, initSpeed):
+        self.__setTimeEvolution(initPos, initSpeed)
+
         plt.plot(self.timeSeq, self.positionSeq)
         plt.xlabel("time in sec")
         plt.ylabel("position")
         plt.show()
 
+    def phasePlot(self, initPos, initSpeed):
+        self.__setTimeEvolution(initPos, initSpeed)
 
-class timeEvolutionFrictionPendulum(timeEvolutionFreePendulum):
-    alpha = 0.1
-
-    def __init__(self, alpha):
-        self.alpha = alpha
-
-        timeEvolutionFreePendulum.__init__(self)
-
-
-class timeEvolutionFrictionAndForce(timeEvolutionFrictionPendulum):
-    f = 1
-    omega = 2
-    tMax = 100
-
-    def __init__(self, alpha, f, omega):
-        self.f = f
-        self.omega = omega
-        timeEvolutionFrictionPendulum.__init__(self, alpha, )
+        plt.plot(self.posSeq, self.speedSeq)
+        plt.xlabel("position")
+        plt.ylabel("speed")
+        plt.show()
 
 
-class timeEvolutionSinForce(timeEvolutionFrictionAndForce):
+class timeEvolutionPendulum(diffEqPendulum):
+    tMax = 10
+
+    self.setTimeEvolution(self.Y_0[0], self.Y_0[1])
+
+    def setTimeEvolution(self, initPos, initSpeed):
+        # calculate position and speed for all timesteps
+
+
+class timeEvolutionSinForce(timeEvolutionPendulum):
     def diffEq(self, t, Y):
         # system of linked differential equations
         F = np.zeros(2)
@@ -99,7 +100,7 @@ class timeEvolutionSinForce(timeEvolutionFrictionAndForce):
         return F
 
 
-class phaseSpaceFreePendulum(timeEvolutionFreePendulum):
+class phaseSpacePendulum(timeEvolutionPendulum):
     noOfSequences = 100
     omega0 = 1
     h = 0.01
@@ -110,7 +111,10 @@ class phaseSpaceFreePendulum(timeEvolutionFreePendulum):
     positionSequences = np.zeros((100, 100))
     speedSequences = np.zeros((100, 100))
 
-    def __init__(self, noOfSequences):
+    def __init__(self, alpha, f, omega, noOfSequences):
+        self.alpha = alpha
+        self.f = f
+        self.omega = omega
         self.noOfSequences = noOfSequences
 
         self.setPhaseSpace(self.noOfSequences)
@@ -140,28 +144,10 @@ class phaseSpaceFreePendulum(timeEvolutionFreePendulum):
         plt.show()
 
 
-class phaseSpaceFrictionPendulum(phaseSpaceFreePendulum):
-    alpha = 0.1
-
-    def __init__(self, alpha, noOfSequences):
-        self.alpha = alpha
-        phaseSpaceFreePendulum.__init__(self, noOfSequences)
-
-
-class phaseSpaceFrictionAndForce(phaseSpaceFrictionPendulum):
-    f = 1
-    omega = 2
-
-    def __init__(self, alpha, f, omega, noOfSequences):
-        self.f = f
-        self.omega = omega
-        phaseSpaceFrictionPendulum.__init__(self, alpha, noOfSequences)
-
-
-class phaseSpaceSinForce(timeEvolutionSinForce, phaseSpaceFrictionAndForce):
+class phaseSpaceSinForce(timeEvolutionSinForce, phaseSpacePendulum):
     # combine the two classes
     def __init__(self, alpha, f, omega, noOfSequences):
-        phaseSpaceFrictionAndForce.__init__(
+        phaseSpacePendulum.__init__(
             self, alpha, f, omega, noOfSequences)
 
 
@@ -203,84 +189,87 @@ class bifurcationPlotSinForce(phaseSpaceSinForce):
 # =============================================================================
 
 class exercises():
+    message = "exercise:"
 
     def __init__(self):
-        print("exercise:")
+        print(self.message)
 
-    def e2a1():
+    def e2a1(self):
         initPos = 0.1
         initSpeed = 0
-        freePendulumSmallDisplacement = timeEvolutionFreePendulum()
+        freePendulumSmallDisplacement = timeEvolutionPendulum(0, 0, 0)
         freePendulumSmallDisplacement.setTimeEvolution(initPos, initSpeed)
         freePendulumSmallDisplacement.timePlot()
 
-    def e2a2():
+    def e2a2(self):
         initPos = 1
         initSpeed = 0
-        freePendulumLargeDisplacement = timeEvolutionFreePendulum()
+        freePendulumLargeDisplacement = timeEvolutionPendulum(0, 0, 0)
         freePendulumLargeDisplacement.setTimeEvolution(initPos, initSpeed)
         freePendulumLargeDisplacement.timePlot()
 
-    def e2a3():
-        oneFreePendulum = phaseSpaceFreePendulum(1)
+    def e2a3(self):
+        oneFreePendulum = phaseSpacePendulum(0, 0, 0, 1)
         oneFreePendulum.phasePlot()
 
-    def e2b():
+    def e2b(self):
         noOfSequences = 100
-        freePendulumPhaseSpace = phaseSpaceFreePendulum(noOfSequences)
+        freePendulumPhaseSpace = phaseSpacePendulum(0, 0, 0, noOfSequences)
         freePendulumPhaseSpace.phasePlot()
 
-    def e2c1():
+    def e2c1(self):
         alpha = 0.1
-        withFriction = timeEvolutionFrictionPendulum(alpha)
+        withFriction = timeEvolutionPendulum(alpha, 0, 0)
         withFriction.timePlot()
 
-    def e2c2():
+    def e2c2(self):
         alpha = 0.1
         noOfSequences = 100
-        withFriction = phaseSpaceFrictionPendulum(alpha, noOfSequences)
+        withFriction = phaseSpacePendulum(alpha, 0, 0, noOfSequences)
         withFriction.phasePlot()
 
-    def e2d1():
+    def e2d1(self):
         alpha = 0.1
         f = 0.2
         omega = 0.6
-        frictionForce = timeEvolutionFrictionAndForce(alpha, f, omega)
+        frictionForce = timeEvolutionPendulum(alpha, f, omega)
         frictionForce.timePlot()
 
-    def e2d2():
+    def e2d2(self):
         alpha = 0.1
         f = 0.2
         omega = 0.6
         noOfSequences = 100
-        frictionForce = phaseSpaceFrictionAndForce(
+        frictionForce = phaseSpacePendulum(
             alpha, f, omega, noOfSequences)
         frictionForce.phasePlot()
 
-    def e2d3():
+    def e2d3(self):
         alpha = 0.1
         f = 0.8
         omega = 0.6
-        frictionForce = timeEvolutionFrictionAndForce(alpha, f, omega)
+        frictionForce = timeEvolutionPendulum(alpha, f, omega)
         frictionForce.timePlot()
 
-    def e2d4():
+    def e2d4(self):
         alpha = 0.1
         f = 0.8
         omega = 0.6
         noOfSequences = 100
-        frictionForce = phaseSpaceFrictionAndForce(
+        frictionForce = phaseSpacePendulum(
             alpha, f, omega, noOfSequences)
         frictionForce.phasePlot()
 
-    def e2e1():
+    def e2e1(self):
         sinForce = phaseSpaceSinForce(0.1, 0.2, 2, 100)
         sinForce.phasePlot()
 
-    def e2e2():
+    def e2e2(self):
         biPlot = bifurcationPlotSinForce()
 
 # ==============================================================================
 
 
-exercises.e2a2()
+exercise = exercises()
+
+exercise.e2a1()
