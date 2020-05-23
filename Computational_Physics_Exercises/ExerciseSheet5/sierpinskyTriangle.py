f8566f2c7ea0c7af160772454386fe07adc04858
Bl4ckP4nther4 <bl4ckp4nther4@gmail.com>
2020-05-23 12:36:52 +0500

    defining class structure

--- Computational_Physics_Exercises/ExerciseSheet5/sierpinskyTriangle.py
+++ Computational_Physics_Exercises/ExerciseSheet5/sierpinskyTriangle.py
@@ -1,25 +1,45 @@
 import numpy as np
 import random as rand
+import matplotlib.pyplot as plt
 
-v_1 = [0, 1]
-v_2 = [1, 0]
-v_3 = [-1, 0]
 
-vertex = np.zeros((2, 3))
-vertex[:, 0] = v_1
-vertex[:, 1] = v_2
-vertex[:, 2] = v_3
+class sierpinskiTriangle():
 
-print(vertex)
+    # number of points inside the triangle
+    N = 10
 
-p = [0.2, 0.2]
+    # vertices; corner points of the triangle
+    vertex = np.zeros((2, 3))
+    vertex[:, 0] = [0, 1]
+    vertex[:, 1] = [1, 0]
+    vertex[:, 2] = [-1, 0]
 
-points = [p]
+    # starting point
+    p = [0.2, 0.2]
 
-for i in range(1, 11):
-    j = int(np.ceil(rand.random()*3) - 1)
-    print(j)
-    p = 1/2 * (p + vertex[:, j])
-    points = np.append(points, p)
+    # points inside the triangle
+    points = np.zeros((2, 11))
 
-print()
+    def __init__(self, N):
+        self.N = N
+        self.setPoints(self.N)
+
+    def setPoints(self, N):
+        # generate points of the sierpinsky triangle
+        self.p = [rand.random(), rand.random()]
+        self.points = np.zeros((2, self.N+1))
+        self.points[:, 0] = self.p
+
+        # generate the points
+        for i in range(1, N):
+            j = int(np.ceil(rand.random()*3) - 1)
+            self.p = 1/2 * (self.p + self.vertex[:, j])
+            self.points[:, i] = self.p
+
+    def plotPoints(self):
+        plt.scatter(self.points[0, :], self.points[1, :], s=0.1)
+        plt.show()
+
+
+ST = sierpinskiTriangle(100000)
+ST.plotPoints()
